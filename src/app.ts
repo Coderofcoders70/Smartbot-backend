55ec9169aec891927bbdebfda6f8a6087234cd75
Coderofcoders70 <Lakshayas918@gmail.com>
2025-12-25 15:55:55 +0530

    Added folders in src

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,13 @@
 import cors from 'cors';
 import express from 'express';
-import chatRoutes from './routes/chat-routes';
 
 console.log('App file loaded');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// app.use((req, _res, next) => {
-//   console.log(`Incoming request: ${req.method} ${req.url}`);
-//   next();
-// });
-
-
-app.use('/api', chatRoutes);
 
 // Health check route
 app.get('/', (_req, res) => {
--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  // console.log(`Server listening on http://localhost:${PORT}`);
 });
